7837cd154552261fe90a469311f96df27f678633
Victor de Souza <buenos.victor2004@gmail.com>
2024-05-05 21:46:09 -0300

    Sim, preciso de commits semanticos
    criei os arquivos iniciais pro codigo do jogo

--- JS/index.js
+++ JS/index.js
@@ -1,38 +1,79 @@
 const front = "cardFront"
 const back = "cardBack"
+const card = "card"
+const icon = "icon"
 
-let techs = [
-    'bootstrap',
-    'css',
-    'eletron',
-    'firebase',
-    'html',
-    'javascript',
-    'jquery',
-    'mongo',
-    'node',
-    'react'
-];
+// let techs = [
+//     'bootstrap',
+//     'css',
+//     'eletron',
+//     'firebase',
+//     'html',
+//     'javascript',
+//     'jquery',
+//     'mongo',
+//     'node',
+//     'react'
+// ];
 
-
-let cards = []
 
 startGame()
 
 function startGame() {
 
-    cards = createCardsFromTechs(techs)
-    schuffleCards(cards)
-    console.log(cards);
+    game.createdCards = game.createCardsFromTechs(game.techs)
+    schuffleCards(game.cards)
+    inicializeCards(game.cards)
+    
+}
+
+function inicializeCards(boardCards){
+    const gameBoard = document.querySelector(".gameBoard")
+    
+    boardCards.forEach( (newCard) => {
+        let cardElement = document.createElement('div')
+        cardElement.id = newCard.id
+        cardElement.classList.add(card)
+        cardElement.dataset.icon = newCard.icon
+
+        createCardContent(newCard, cardElement)
+        cardElement.addEventListener('click', flipCard)
+        gameBoard.appendChild(cardElement)
+    });
+
 
 }
 
-//não entendi o direito daqui
+function createCardContent(cardContainer,cardAttribute){
+    createCardFace(front, cardContainer,cardAttribute)
+    createCardFace(back, cardContainer,cardAttribute)
+
+
+}
+
+function createCardFace(face, cardImg, element){
+    let cardElementFace = document.createElement('div')
+    cardElementFace.classList.add(face)
+    if(face === front){
+        let iconElement = document.createElement('img')
+        iconElement.classList.add(icon)
+        iconElement.src = "./images/" + cardImg.icon + ".png"
+        cardElementFace.appendChild(iconElement)
+    }else{
+        cardElementFace.innerHTML = "&lt/&gt"
+    }
+
+    element.appendChild(cardElementFace)
+}
+
+function flipCard(){
+    this.classList.add("flip")
+}
 
 
 function schuffleCards(shuffledCards) {
 
-    let currentIndex = cards.length;
+    let currentIndex = game.cards.length;
     let randomIndex = 0;
 
     while (currentIndex !== 0) {
@@ -40,38 +81,38 @@ function schuffleCards(shuffledCards) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex--;
 
-        //até aqui
 
         [shuffledCards[randomIndex], shuffledCards[currentIndex]] = [shuffledCards[currentIndex], shuffledCards[randomIndex]]
     }
 
 }
 
+// sera q eu n deveria mudar o nome do parametro "techs"??
 
-function createCardsFromTechs(techs) {
+// function createCardsFromTechs(techs) {
 
-    const createdCards = []
+//     const createdCards = []
 
-    for (let tech of techs)
-    createdCards.push(createPairFromTech(tech))
+//     for (let tech of techs)
+//     createdCards.push(createPairFromTech(tech))
 
-    return(createdCards.flatMap(pair => pair));
-}
+//     return(createdCards.flatMap(pair => pair));
+// }
 
-function createPairFromTech(tech) {
-    return [{
-        class: createClassWithTech(tech),
-        icon: tech,
-        flipped: false
-    },
-    {
-        class: createClassWithTech(tech),
-        icon: tech,
-        flipped: false
-    }
-    ]
-}
+// function createPairFromTech(tech) {
+//     return [{
+//         class: createClassWithTech(tech),
+//         icon: tech,
+//         flipped: false
+//     },
+//     {
+//         class: createClassWithTech(tech),
+//         icon: tech,
+//         flipped: false
+//     }
+//     ]
+// }
 
-function createClassWithTech(tech) {
-    return tech + parseInt(Math.random() * 100)
-}
+// function createClassWithTech(tech) {
+//     return tech + parseInt(Math.random() * 100)
+// }
