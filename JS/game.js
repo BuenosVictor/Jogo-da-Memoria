a6fea34f5ead8e9d88e2f4b41aaf9d9193c570e8
Victor de Souza <buenos.victor2004@gmail.com>
2024-05-24 05:16:38 -0300

    refatoração e separação do tabuleiro
    neste commit refatorei e separei o codigo do tabuleiro para que esteja
    em um local diferente da logica do jogo em si, tambem modifiquei alguns arquivos CSS
    para manter a responsividade e melhorar a experiencia do usuario

--- JS/game.js
+++ JS/game.js
@@ -1,51 +1,120 @@
-// let game = {
+let game = {
 
-//     techs: [
-//         'bootstra1p',
-//         'css',
-//         'eletron',
-//         'firebase',
-//         'html',
-//         'javascript',
-//         'jquery',
-//         'mongo',
-//         'node',
-//         'react'
-//     ],
+    front : "cardFront",
+    back : "cardBack",
+    card : "card",
+    icon : "icon",
+    gameBoard : document.querySelector(".gameBoard"),
 
-//     cards: [],
+    cards: [],
 
-//     createCardsFromTechs: function () {
+    techs: [
+        'bootstrap',
+        'css',
+        'electron',
+        'firebase',
+        'html',
+        'javascript',
+        'jquery',
+        'mongo',
+        'node',
+        'react'
+    ],
 
-//         const createdCards = []
+    inicializeCards:function (boardCards){
+        const gameBoard = document.querySelector(".gameBoard")
+        
+        boardCards.forEach( (newCard) => {
+            let cardElement = document.createElement('div')
+            cardElement.classList.add(this.card)
+            cardElement.id = newCard.id
+            cardElement.dataset.icon = newCard.icon
+            cardElement.addEventListener('click', this.flipCard)
+    
+            this.createCardContent(newCard, cardElement)
+    
+            gameBoard.appendChild(cardElement)
+        });
+    
+    
+    },
+    
+    createCardContent:function (cardContainer,cardAttribute){
+        this.createCardFace(this.front, cardContainer,cardAttribute)
+        this.createCardFace(this.back, cardContainer,cardAttribute)
+    
+    
+    },
+    
+    createCardFace: function (face, cardImg, element){
+        let cardElementFace = document.createElement('div')
+        cardElementFace.classList.add(face)
+        if(face === this.front){
+            let iconElement = document.createElement('img')
+            iconElement.classList.add(this.icon)
+            iconElement.src = "./images/" + cardImg.icon + ".png"
+            cardElementFace.appendChild(iconElement)
+        }else{
+            cardElementFace.innerHTML = "&lt/&gt"
+        }
+    
+        element.appendChild(cardElementFace)
+    
+    },
 
-//         this.techs.forEach( (tech) =>{
-//             createdCards.push(createPairFromTech(tech))
-//         })
-            
-//         return (createdCards.flatMap(pair => pair));
-//     },
 
-//     createPairFromTech: function (forEachPair) {
-//         return [{
-//             class: createIdWithTech(forEachPair),
-//             icon: forEachPair,
-//             flipped: false
-//         },
-//         {
-//             class: createIdWithTech(forEachPair),
-//             icon: forEachPair,
-//             flipped: false
-//         }
-//         ]
-//     },
+    shuffleCards: function (shuffledCards) {
 
-//     createIdWithTech: function createIdWithTech(language) {
-//         return language + parseInt(Math.random() * 100)
-//     }
+        let currentIndex = this.cards.length;
+        let randomIndex = 0;
+
+        while (currentIndex !== 0) {
+
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
 
 
+            [shuffledCards[randomIndex], shuffledCards[currentIndex]] = [shuffledCards[currentIndex], shuffledCards[randomIndex]]
+        }
+
+    },
+
+    createCardsFromTechs: function () {
+
+        this.techs.forEach((tech) => {
+            this.cards.push(...this.createPairFromTech(tech))
+        })
+
+        return (this.cards.flatMap(pair => pair));
+    },
+
+    createPairFromTech: function (forEachPair) {
+        return [{
+            id: this.createidWithTech(forEachPair),
+            icon: forEachPair,
+            flipped: false,
+        },
+        {
+            id: this.createidWithTech(forEachPair),
+            icon: forEachPair,
+            flipped: false,
+        }
+        ]
+    },
+
+    createidWithTech: function (language) {
+        return language + parseInt(Math.random() * 100) 
+    },
+
+    flipCard:function (){
+        this.classList.add("flip")
+    },
+
+    resetGameBoard: function () {
+        gameBoard = document.querySelector('.gameBoard');
+        gameBoard.innerHTML = '';
+    },
 
 
+}
 
-// }
